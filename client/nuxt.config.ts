510a5136a336d56ed3d27714338fd9ba78c3985d
ARPCharlie <charlie@arp-lan.com>
2024-12-23 23:15:05 +0100

    Feat: Tailwindcss

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  css: ['~/assets/index.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   devtools: { enabled: true }
 })
