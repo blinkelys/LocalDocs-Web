37676db35a197cd4f936c9281ad7f87e27c232df
ARPCharlie <charlie@arp-lan.com>
2025-01-09 12:25:58 +0100

    Feat: doc deletion

--- client/pages/docs.vue
+++ client/pages/docs.vue
@@ -3,7 +3,7 @@
         <h1 class="text-4xl font-bold">Docs</h1>
 
         <div class="w-full md:w-3/4 h-fit p-8 my-10 bg-gray-800 flex flex-col items-start justify-start text-white">
-            <div v-for="doc in docs" :key="doc.created_at" class="mb-6">
+            <div v-for="doc in docs" :key="doc._id" class="mb-6">
                 <h2 class="text-2xl font-semibold">{{ doc.title }}</h2>
                 <p class="text-lg">{{ doc.description }}</p>
                 <div class="mt-2">{{ doc.content }}</div>
@@ -11,6 +11,7 @@
                     <span v-for="tag in doc.tags" :key="tag" class="bg-gray-600 text-white rounded-full px-2 py-1 text-sm mr-2">{{ tag }}</span>
                 </div>
                 <p class="text-sm text-gray-400">Created at: {{ doc.createdAt }} | Updated at: {{ doc.updatedAt }}</p>
+                <button @click="deleteDoc(doc._id)" class="text-red-500 font-bold">Delete</button>
             </div>
         </div>
     </div>
@@ -27,9 +28,22 @@
         },
         mounted() {
             axios.get('http://localhost:5000/docs')
-            .then(response => {
-                this.docs = response.data;
-            });
+                .then(response => {
+                    this.docs = response.data;
+                    console.log(this.docs);
+                });
+        },
+        methods: {
+            deleteDoc(docId) {
+                axios.delete(`http://localhost:5000/docs/${docId}`)
+                    .then(response => {
+                        console.log(response.data);
+                        this.docs = this.docs.filter(doc => doc._id !== docId);
+                    })
+                    .catch(error => {
+                        console.error('Error deleting document:', error);
+                    });
+            }
         }
     }
 </script>
--- server/src/routes/docs.ts
+++ server/src/routes/docs.ts
@@ -11,7 +11,19 @@ router.get("/", async (req: express.Request, res: express.Response) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: "Internal Server Error" });
-    }
-})
+    };
+});
+
+router.delete("/:id", async (req: express.Request, res: express.Response) => {
+    console.log("Deleting docs")
+    try {
+        const docs = await Doc.deleteOne({ _id: req.params.id });
+        res.status(200).json(docs);
+        console.log("Docs deleted")
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
+});
 
 module.exports = router
